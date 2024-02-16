2bf6593c4d1c370460a52516a86ab93ce2647e3f
INSU <tonuto@naver.com>
2024-02-16 23:03:35 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# jenkins sample boot
+
+​	젠킨스 배포 샘플 스프링 부트입니다.
---
+++ src/main/java/org/example/jenkinsboot/JenkinsBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.jenkinsboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsBootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JenkinsBootApplication.class, args);
+  }
+
+}
